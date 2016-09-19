16371887f7e62b99ee0a9063e0475f1f49306ac7
Martin Torp <toldamtorp@gmail.com>
2016-09-19 21:08:01 +0200

    Try delete file before reducing

--- delta-multifile.js
+++ delta-multifile.js
@@ -13,15 +13,11 @@ const path = require("path"),
 
 var dir, 
     mainFile, 
+    mainFileTmpDir,
     predicate,
     fileUnderTest,
-    tmpDir;
-
-function createAndInstantiateDeltaDir() {
-    tmpDir = fs.mkdtempSync(config.tmp_dir + "/jsdelta-multifile");
-    fs.copySync(dir, tmpDir);
-    return tmpDir;
-}
+    tmpDir,
+    backupFile;
 
 /** 
   * Recursively pass through the file-hierarchy and invoke deltalib.main on the files
@@ -49,17 +45,25 @@ function deltaDebug(file) {
             replay_idx : -1,
 	    multifile_mode : true
         };
-	console.log("Reducing " + path.relative(tmpDir, file));
-        deltalib.main(options);
+
+	//Try removing fileUnderTest completely before delta-debugging
+	fs.copySync(fileUnderTest, backupFile);      	
+	fs.unlink(fileUnderTest);
+
+	//Reduce fileUnderTest if predicate fails without fileUndeTest
+	if (!predicate.test(mainFileTmpDir)) {
+	    fs.copySync(backupFile, fileUnderTest);      	
+	    console.log("Reducing " + path.relative(tmpDir, fileUnderTest));
+	    deltalib.main(options);
+	}
     }
 }
 
 var predicate_wrapper = {
     test: function (deltaReducedFile) {
-	var backupFile = path.resolve(tmpDir, "tmp_file.js");
 	fs.copySync(fileUnderTest, backupFile);
 	fs.copySync(deltaReducedFile, fileUnderTest);
-	var mainFileTmpDir = path.resolve(tmpDir, mainFile);
+	mainFileTmpDir = path.resolve(tmpDir, mainFile);
 	var res = predicate.test(mainFileTmpDir);
 
 	//Restore backed-up file if new version fails the predicate
@@ -75,15 +79,13 @@ function main () {
     parseOptions();
     checkOptions();
     createAndInstantiateDeltaDir();
+    createTmpBackupFile();
+
+    //Begin
     deltaDebug(tmpDir);
-    cleanup();
     console.log("Minimized version available at " + tmpDir);
 }
 main();
-
-function cleanup() {
-    fs.unlink(path.resolve(tmpDir, "tmp_file.js"));
-}
 
 function parseOptions() {
     var args = process.argv;
@@ -106,6 +108,19 @@ function checkOptions() {
    catch (err) {
        logAndExit("Could not find main file " + mainFilePath);
    }
+}
+
+function createTmpBackupFile() {
+    var tmpBackupDir = fs.mkdtempSync(config.tmp_dir + "/backup");
+    backupFile = path.resolve(tmpBackupDir, "backup");
+    console.log(backupFile);
+}
+
+function createAndInstantiateDeltaDir() {
+    tmpDir = fs.mkdtempSync(config.tmp_dir + "/jsdelta-multifile");
+    fs.copySync(dir, tmpDir);
+    mainFileTmpDir = path.resolve(tmpDir, mainFile);
+    return tmpDir;
 }
 
 function logAndExit(msg) {
