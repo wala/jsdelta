88a7017ffba8e06d9e27ce07abb4b1b2a9f4a872
Martin Torp <toldamtorp@gmail.com>
2016-09-19 17:45:15 +0200

    Add a test sample

---
+++ pred.js
@@ -0,0 +1,19 @@
+var execSucc = function(filename) {
+    var process = require("child_process");
+
+    const options = {
+        stdio: "pipe"
+    };
+
+    try {
+        output = process.execSync('node ' + filename, options);
+    } catch (err) {
+        return false;
+    }
+
+    if (output === null) {
+        return false;
+    }
+    return output.indexOf("success") !== -1;
+}
+exports.test = execSucc;
