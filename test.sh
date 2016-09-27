c43928bcb62154810dfd33cf331c4726f7fc495f
Martin Torp <toldamtorp@gmail.com>
2016-09-27 12:57:29 +0200

    add small test script

---
+++ test.sh
@@ -0,0 +1,4 @@
+./delta.js $(pwd)/examples/simple/main.js ./examples/pred.js
+./delta.js --dir $(pwd)/examples/multi-simple to_minimize.js ./examples/pred.js
+./delta.js --dir $(pwd)/examples/multi-advanced main-file-folder/main.js ./examples/pred.js
+
