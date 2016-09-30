b2103cb7d9e754b6b30595a4563a1fb718ea8234
Martin Torp <toldamtorp@gmail.com>
2016-09-30 14:15:02 +0200

    fail on error in test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+set -e
 ./delta.js examples/simple/main.js ./examples/pred.js
 ./delta.js --dir examples/multi-simple to_minimize.js ./examples/pred.js
 ./delta.js --dir examples/multi-advanced main-file-folder/main.js ./examples/pred.js
