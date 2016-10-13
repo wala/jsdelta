705a9a8662fedcd374344d884f87945a597dd0a9
Martin Torp <toldamtorp@gmail.com>
2016-10-13 13:09:37 +0200

    fix bug in test

--- examples/predicates/check-result.js
+++ examples/predicates/check-result.js
@@ -1,4 +1,4 @@
-exports.cmd = "examples/predicates/check-result-cmd-stderr.js";
+exports.cmd = "examples/predicates/check-result-cmd.js";
 
 exports.checkResult = function (errCode, stdout, stderr, time) {
     return errCode == 0;
