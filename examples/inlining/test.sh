44d0d424a674ea9fe22263c2228f0214b79e7bdd
Esben Andreasen <esbenandreasen@gmail.com>
2017-01-09 23:33:41 +0100

    simplification of test

--- examples/inlining/test.sh
+++ examples/inlining/test.sh
@@ -4,19 +4,17 @@ source "${MAIN_FILE_FOLDER}/../../util/example_setup.sh";
 PREDICATE="${ROOT}/examples/predicates/pred.js";
 
 #Run delta.js
-${ROOT}/delta.js --optimize --out ${TMP_OUT} --dir ${MAIN_FILE_FOLDER} inlining.js ${PREDICATE} >/dev/null
+INLINED=${TMP_OUT}/inlined.js
+${ROOT}/delta.js --optimize --out ${INLINED} ${MAIN_FILE_FOLDER}/inlining.js ${PREDICATE} >/dev/null
 
 # Compare with unoptimized
-TMP_OUT_NO_OPTIMIZATION="${TMP_OUT}_no-opt"
-mkdir -p ${TMP_OUT_NO_OPTIMIZATION}
-${ROOT}/delta.js --out ${TMP_OUT_NO_OPTIMIZATION} --dir ${MAIN_FILE_FOLDER} inlining.js ${PREDICATE} >/dev/null
+NOT_INLINED=${TMP_OUT}/not-inlined.js
+${ROOT}/delta.js --out ${NOT_INLINED} ${MAIN_FILE_FOLDER}/inlining.js ${PREDICATE} >/dev/null
 
 set +e;
-${ROOT}/util/cmp-size.js ${TMP_OUT} ${TMP_OUT_NO_OPTIMIZATION};
+${ROOT}/util/cmp-size.js ${INLINED} ${NOT_INLINED};
 EXIT_CODE=$?;
 set -e;
-
-rm -rf ${TMP_OUT_NO_OPTIMIZATION}
 
 # Fail if optimized output is not smaller than unoptimized output
 if [[ ${EXIT_CODE} -ne 0 ]]; then
